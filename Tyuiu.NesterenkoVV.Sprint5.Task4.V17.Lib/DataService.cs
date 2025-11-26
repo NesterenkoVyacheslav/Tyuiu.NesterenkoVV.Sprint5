f37e8fb9e694a936854f0f6b5bf2cf6d5b31df73
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-11-26 16:57:56 +0500

    Task4

--- Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Lib/DataService.cs
+++ Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Lib/DataService.cs
@@ -6,7 +6,8 @@ namespace Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double x = double.Parse(strx);
+            strx = strx.Replace(".", ",");
+            double x = Convert.ToDouble(strx);
             double y = Math.Round(Math.Sin(2 / (3 * x)) + Math.Pow(x, 2), 3);
             return y;
         }
