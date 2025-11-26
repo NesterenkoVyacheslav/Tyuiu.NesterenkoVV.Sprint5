51cdb52309e3e16ffefbe988a55617f883d6eda6
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-11-26 16:47:20 +0500

    Task4

---
+++ Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Lib
+{
+    public class DataService : ISprint5Task4V17
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+            double x = double.Parse(strx);
+            double y = Math.Round(Math.Sin(2 / (3 * x)) + Math.Pow(x, 2), 3);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint5.Task4.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V17.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool FileExist = File.Exists(path);
+            bool wait = true;
+            Assert.AreEqual(wait, FileExist);
+        }
+    }
+}
