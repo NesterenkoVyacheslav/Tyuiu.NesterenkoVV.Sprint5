2bed2382026472b90152c86b2eb799e6e9a558a3
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-11-27 19:34:41 +0500

    Task7

---
+++ Tyuiu.NesterenkoVV.Sprint5.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.NesterenkoVV.Sprint5.Task7.V1.Lib
+{
+    public class DataService : ISprint5Task7V1
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string temppath = Path.GetTempPath();
+            string newpath = Path.Combine(temppath, "OutPutFileTask7.txt");
+            FileInfo fileinfo = new FileInfo(newpath);
+
+            if (fileinfo.Exists )
+            {
+                File.Delete(newpath);
+            }
+
+            string text = File.ReadAllText(path);
+            string newtext = "";
+            foreach (char c in text) 
+            {
+                if (!char.IsDigit(c)) 
+                {
+                newtext += c;
+                }
+            }
+            File.AppendAllText(newpath, newtext);
+            return newpath;
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint5.Task7.V1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.NesterenkoVV.Sprint5.Task7.V1.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint5.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Нестеренко В.В. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема:  Чтение данных из текстового файла                                *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПб-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Удалить все цифры из файла                                              *");
+            Console.WriteLine("*                                                                          ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            string path = @"C:\DataSprint5\InPutDataFileTask7V1.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string res = ds.LoadDataAndSave(path);
+            Console.WriteLine(res);
+        }
+    }
+}
